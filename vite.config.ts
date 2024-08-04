af1620685fc84fc95508422ebcb68e3e7f2ebac8
kayle <215031463@qq.com>
2024-08-04 17:47:01 +0800

    chorm: 增加alias配置，css reset

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@import "@scss/variables.scss";',
+				javascriptEnabled: true,
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@scss': path.resolve(__dirname, 'src/assets/scss'),
+			'@images': path.resolve(__dirname, 'src/assets/images'),
+			'@views': path.resolve(__dirname, 'src/views'),
+			'@services': path.resolve(__dirname, 'src/services'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
+		},
+	},
+});
